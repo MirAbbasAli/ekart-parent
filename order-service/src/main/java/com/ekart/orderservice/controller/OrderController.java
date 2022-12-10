b76bf383e32b14666629569a2857ba0a258b428e
MirAbbasAli <abbasalimir97@gmail.com>
2022-12-10 17:09:50 +0530

    Add Resilience4j and introduce fallback method in order-service

--- order-service/src/main/java/com/ekart/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/ekart/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.ekart.orderservice.controller;
 
 import com.ekart.orderservice.dto.OrderRequest;
 import com.ekart.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +15,13 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name="inventory", fallbackMethod = "placeOrderFallback")
     public String placeOrder(@RequestBody OrderRequest orderRequest){
         orderService.placeOrder(orderRequest);
         return "Order Placed Successfully";
+    }
+
+    public String placeOrderFallback(OrderRequest orderRequest, RuntimeException runtimeException){
+        return "Oops! Something went wrong, please order after some time!";
     }
 }
